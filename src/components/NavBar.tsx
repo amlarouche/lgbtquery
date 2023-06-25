f3972a4ec903740287f4ba5b076233b6f43488f1
Emily Chu <lin.emily.chu@gmail.com>
2023-06-25 11:05:33 -0500

    styling updates

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,29 +3,31 @@ import Link from "next/link"
 
 export default function NavBar({ isHomepage }: { isHomepage: boolean }) {
   const newSearch = (
-    <Text>
+    <Text className="nav-link" fontSize="20px">
       <Link href="/">New Search</Link>
     </Text>
   )
   const contact = (
-    <Text>
+    <Text fontSize="20px" className="nav-link">
       <Link href="/">Contact</Link>
     </Text>
   )
+
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16)
   return (
     <Flex
       className="header"
       padding="32px"
       align="center"
-      justify="center"
+      justify={isHomepage ? "center" : "space-between"}
       height="20px"
       fontSize="80px"
     >
-      {isHomepage ? newSearch : null}
-      <Heading fontSize="60px" fontFamily={"'Caprasimo', cursive"}>
+      {!isHomepage ? newSearch : null}
+      <Heading color={"#" + randomColor} fontSize="60px" fontFamily={"'Caprasimo', cursive"}>
         LGBTQuery
       </Heading>
-      {isHomepage ? contact : null}
+      {!isHomepage ? contact : null}
     </Flex>
   )
 }
