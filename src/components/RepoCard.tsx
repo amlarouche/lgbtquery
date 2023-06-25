5eebae6c646605664af5fedd26b8a5886911bf2d
Emily Chu <lin.emily.chu@gmail.com>
2023-06-24 23:16:41 -0500

    styling

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,21 +1,40 @@
-import { Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { Button, Flex, Heading, Image, Text } from "@chakra-ui/react"
+import Link from "next/link"
+import { useRouter } from "next/router"
 
 export default function RepoCard({ repo }: { repo: Record<string, any> }) {
-    const router = useRouter()
+  const router = useRouter()
 
-    const handleClick = async (owner: string, name: string) => {
-        await router.push(`/results/${owner}_${name}`)
-    }
+  const handleClick = async (owner: string, name: string) => {
+    await router.push(`/results/${owner}_${name}`)
+  }
 
-    return (
-        <Flex flexDir="column" align="center" justify="center" width="40%" border={"1px solid black"} borderRadius={"8px"} padding={"32px"} m="16px">
-            <Text>Name: {repo.name}</Text>
-            <Text><Link href={repo.html_url}>Repository</Link></Text>
-            <Text>Description: {repo.description}</Text>
-            <Button onClick={() => handleClick(repo.owner, repo.name)}>See Contributors</Button>
-            <Text>Owner: {repo.owner}</Text>
-        </Flex>
-    )
+  return (
+    <Flex
+      flexDir="column"
+      align="center"
+      justify="center"
+      width="40%"
+      height="50%"
+      border={"1px solid white"}
+      borderRadius={"8px"}
+      padding={"32px"}
+      m="16px"
+      backgroundColor={"white"}
+    >
+      <Text>Name: {repo.name}</Text>
+      <Text>
+        <Link href={repo.html_url}>Repository</Link>
+      </Text>
+      <Text>Description: {repo.description}</Text>
+      <Button
+        backgroundColor={"rgba(109, 209, 178,0.4)"}
+        className={"contribute-button"}
+        onClick={() => handleClick(repo.owner, repo.name)}
+      >
+        See Contributors
+      </Button>
+      <Text>Owner: {repo.owner}</Text>
+    </Flex>
+  )
 }
--- src/components/ReposDisplay.tsx
+++ src/components/ReposDisplay.tsx
@@ -1,10 +1,23 @@
-import { Flex, Heading, Image, Text } from "@chakra-ui/react";
-import RepoCard from "./RepoCard";
+import { Flex, Heading, Image, Text } from "@chakra-ui/react"
+import RepoCard from "./RepoCard"
 
 export default function ReposDisplay({ repos }: { repos: Record<string, any>[] }) {
-    return (
-        <Flex maxHeight={"50vh"} overflow={"scroll"} flexWrap={"wrap"} m="8px" border={"1px solid black"} borderRadius={"8px"} width={"70%"} padding="32px" align="center" justify="center">
-            {repos.map((repo: Record<string, any>) => <RepoCard key={repo.name} repo={repo} />)}
-        </Flex>
-    )
+  return (
+    <Flex
+      height={"50vh"}
+      flexWrap={"wrap"}
+      m="8px"
+      border={"1px solid rgba(1, 65, 255, 0.2)"}
+      borderRadius={"8px"}
+      width={"70%"}
+      padding="32px"
+      align="center"
+      justify="center"
+      className={"repo-box"}
+    >
+      {repos.map((repo: Record<string, any>) => (
+        <RepoCard key={repo.name} repo={repo} />
+      ))}
+    </Flex>
+  )
 }
